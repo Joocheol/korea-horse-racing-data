575900eba8d0ac8bca505c279c0caf7802a49afe
Joocheol <joocheol@yonsei.ac.kr>
2026-08-20 11:56:25 +0900

    수집: API227 실제 요청주소 적용

--- src/kra_data/config.py
+++ src/kra_data/config.py
@@ -37,7 +37,7 @@ ENDPOINTS: dict[str, Endpoint] = {
         "entries", "API26_2", "API26_2/entrySheet_2", (None,)
     ),
     "results": Endpoint(
-        "results", "API227", "racedetailresult", (None,), response_format="xml"
+        "results", "API227", "racedetailresult/getracedetailresult", (None,), response_format="xml"
     ),
     "race_record": Endpoint(
         "race_record", "API4_3", "API4_3/raceResult_3", (None,), response_format="xml"
